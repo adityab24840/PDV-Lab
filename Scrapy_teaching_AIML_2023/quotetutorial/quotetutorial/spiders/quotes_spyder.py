0ce7e4ab176442e63df5ade09ac6cde4288519a0
Aditya N Bhatt <adityab24840@gmail.com>
2023-08-25 10:49:48 +0530

    Update quotes_spyder.py

--- Scrapy_teaching_AIML_2023/quotetutorial/quotetutorial/spiders/quotes_spyder.py
+++ Scrapy_teaching_AIML_2023/quotetutorial/quotetutorial/spiders/quotes_spyder.py
@@ -12,13 +12,20 @@ class QuotesSpider(scrapy.Spider):
     allowed_domains = ["quotes.toscrape.com"]
     start_urls = ('http://quotes.toscrape.com/',)
     
+    
     def parse(self, response):
-        title = response.css('title::text').extract_first()
-        authors = response.css('.author::text').extract_first()
-        tags = response.css('.tag::text').extract_first()
-
-        yield {
-            'title': title,
-            'authors': authors,
-            'tags': tags
-        }
+        title = response.css('title::text').extract()  #output= string
+        yield {'titletest': title}
+        
+        all_div_quotes = response.css('div.quote')
+        quote_details = []
+        items = {}
+        for quote in all_div_quotes:
+            quotes = quote.css('span.text::text').extract()
+            authors = quote.css('.author::text').extract()
+            tags = quote.css('.tag::text').extract()
+            items['quotes'] = quotes
+            items['authors'] = authors
+            items['tags'] = tags
+            quote_details.append(items)
+        yield {'Quote_List':quote_details}
